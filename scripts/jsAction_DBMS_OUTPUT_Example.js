aeaa233a33aedf2bbddc96b618d2be0807298928
mutlu erturk <45595664+emutert@users.noreply.github.com>
2023-07-21 12:36:52 +0100

    Update and rename jsAction_1.js to jsAction_DBMS_OUTPUT_Example.js

--- scripts/jsAction_DBMS_OUTPUT_Example.js
+++ scripts/jsAction_DBMS_OUTPUT_Example.js
@@ -6,6 +6,12 @@ var TYPES = Packages.oracle.jdbc.OracleTypes;
 //get connection
 var conn = context.currentVU.get('Connection_name_used_in_sql_connection');
 
+
+//dbms_output initialization
+var dbms_output = new DbmsOutput(conn);
+//enable dbms_output
+dbms_output.enable(1000000);
+
 // call object initialization
 var sql = "begin SCHEMA_NAME.PROCDURE_NAME(first_parameter=>?,second_parameter=>?); end;";
 var cs = conn.prepareCall(sql);
@@ -14,6 +20,8 @@ cs.setString(1,'first_parameter_value');
 cs.registerOutParameter(2,TYPES.CURSOR);
 
 cs.execute();
+//dbms_output.show();
+dbms_output.show();
 
 //output the result
 
@@ -22,6 +30,10 @@ var result = cs.getCursor(2);
 while (result.next()) {
   logger.debug(result.getString(1));
 }
+
+
+//close dbms_output
+dbms_output.close();
 
 //close resultset
 result.close();
